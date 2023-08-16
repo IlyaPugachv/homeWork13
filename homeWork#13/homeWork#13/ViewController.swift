f40b786b2671b17209e2985270b4c1aeaf11330c
Ilya Pugachev <ilyapugachv@gmail.com>
2023-08-16 12:46:04 +0300

    Project created

---
+++ homeWork#13/homeWork#13/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homeWork#13
+//
+//  Created by Илья Пугачёв on 16.08.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

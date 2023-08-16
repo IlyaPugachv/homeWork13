69c9b767172f4e6da3a54f2bc731bf3e8170daca
Ilya Pugachev <126100191+IlyaPugachv@users.noreply.github.com>
2023-08-16 14:12:27 +0300

    update some logic

--- homeWork#13/homeWork#13/ViewController.swift
+++ homeWork#13/homeWork#13/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
       // task numbre one
-//         task number two
+//         task number tree
     }
 
 
